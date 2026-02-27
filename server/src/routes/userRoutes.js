770cc502df3b308593ccf56cd6a3bb24bb740c37
Dhruv110904 <jaindhruv110904@gmail.com>
2026-02-28 02:28:48 +0530

    Implemented JWT auth middleware and protected profile route

---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const protect = require('../middleware/authMiddleware');
+
+router.get('/profile', protect, (req, res) => {
+    res.status(200).json({
+        message: "Profile fetched",
+        user: req.user
+    });
+});
+
+module.exports = router;
